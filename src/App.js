7dd0125fec037c2382df88b3e55645ae5286b8a9
Atul Joshi Francis <jo.at.atuljoshif@gmail.com>
2024-08-24 20:01:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Everyone!</h1>
+      <p>This website is going to uncover a lot of insights regarding the field of IoT!</p>
+    </div>
+  );
+}
+
+export default App;
