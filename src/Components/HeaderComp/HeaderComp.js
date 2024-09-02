602ad92a58007be8ad6795df6f24e3ad282cb8ff
Atul Joshi Francis <jo.at.atuljoshif@gmail.com>
2024-09-02 23:14:05 +0530

    HeaderResponsiveness

--- src/Components/HeaderComp/HeaderComp.js
+++ src/Components/HeaderComp/HeaderComp.js
@@ -1,14 +1,19 @@
-import './HeaderComp.css'
+import './HeaderComp.css';
+import menu from '../../assets/menu.svg'
 
 const HeaderComp = () => {
-    return <div className="header">
-        <h1 className="header-heading">IoT Engineer</h1>
-        <div className="header-icons">
-            <a className="header-icon" href='/IoT'>HOME</a>
-            <a className="header-icon" href='/IoT/about'>ABOUT</a>
-            <a className="header-icon" href='/IoT/contact'>CONTACT</a>
+    return (
+        <div className="header">
+            <h1 className="header-heading">IoT Engineer</h1>
+            <input type="checkbox" id="menu-toggle" className="menu-toggle"/>
+            <label htmlFor="menu-toggle" className="menu-icon"><img className='menu-icon-image' src={menu} alt='menu' /></label>
+            <div className="header-icons">
+                <a className="header-icon" href='/IoT'>HOME</a>
+                <a className="header-icon" href='/IoT/about'>ABOUT</a>
+                <a className="header-icon" href='/IoT/contact'>CONTACT</a>
+            </div>
         </div>
-    </div>
-}
+    );
+};
 
 export default HeaderComp;
